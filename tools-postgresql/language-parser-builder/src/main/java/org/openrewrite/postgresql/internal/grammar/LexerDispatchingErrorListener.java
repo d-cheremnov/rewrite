d2ac106d52e5f15834dd1b154d871e7e6ab52482
d.cheremnov <d.cheremnov@asvoip.com>
2023-01-08 11:44:52 +0700

    1 source from https://github.com/antlr/grammars-v4/tree/master/sql/postgresql/Java

---
+++ tools-postgresql/language-parser-builder/src/main/java/org/openrewrite/postgresql/internal/grammar/LexerDispatchingErrorListener.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//Copied from https://github.com/antlr/grammars-v4/tree/master/sql/postgresql/Java
+package org.openrewrite.postgresql.internal.grammar;
+
+import java.util.BitSet;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.*;
+import org.antlr.v4.runtime.misc.*;
+
+public class LexerDispatchingErrorListener implements ANTLRErrorListener
+{
+    Lexer _parent;
+
+    public LexerDispatchingErrorListener(Lexer parent)
+    {
+        _parent = parent;
+    }
+
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+    {
+        var foo = new ProxyErrorListener(_parent.getErrorListeners());
+        foo.syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
+    }
+
+    public void reportAmbiguity(Parser recognizer,
+                                DFA dfa,
+                                int startIndex,
+                                int stopIndex,
+                                boolean exact,
+                                BitSet ambigAlts,
+                                ATNConfigSet configs)
+    {
+        var foo = new ProxyErrorListener(_parent.getErrorListeners());
+        foo.reportAmbiguity(recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs);
+    }
+
+    public void reportAttemptingFullContext(Parser recognizer,
+                                            DFA dfa,
+                                            int startIndex,
+                                            int stopIndex,
+                                            BitSet conflictingAlts,
+                                            ATNConfigSet configs)
+    {
+        var foo = new ProxyErrorListener(_parent.getErrorListeners());
+        foo.reportAttemptingFullContext(recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs);
+    }
+
+    public void reportContextSensitivity(Parser recognizer,
+                                         DFA dfa,
+                                         int startIndex,
+                                         int stopIndex,
+                                         int prediction,
+                                         ATNConfigSet configs)
+    {
+        var foo = new ProxyErrorListener(_parent.getErrorListeners());
+        foo.reportContextSensitivity(recognizer, dfa, startIndex, stopIndex, prediction, configs);
+    }
+}
